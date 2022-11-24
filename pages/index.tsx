5329282ff085f51ced5dc08c03924310bb329bb9
Min ho Kim <minho42@gmail.com>
2022-11-25 02:33:03 +1100

    style

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,30 +38,43 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center space-y-3">
         <article className="flex flex-col w-full items-center justify-center space-y-3">
-          <h2 className="w-full font-semibold rounded-md bg-purple-200 text-center py-1">Fuel Check</h2>
+          <h2 className="w-full font-semibold rounded-md bg-gray-200 text-center py-1">Fuel Check</h2>
           <div className="flex w-full items-center justify-center space-x-4">
-            <div className="text-center text-xl text-gray-500 rounded-xl py-3 bg-gray-100 w-1/3">
-              P91: <span className="text-black">${priceE10}</span>
+            <div className="text-center text-xl rounded-xl py-3 border-2 border-black w-1/3 bg-green-100">
+              <div className="text-gray-600 ">E10</div>
+              <div className="font-semibold">${priceE10}</div>
             </div>
-            <div className="text-center text-xl text-gray-500 rounded-xl py-3 bg-gray-100 w-1/3">
-              P95: <span className="text-black">${price95}</span>
+            <div className="text-center text-xl rounded-xl py-3 border-2 border-black w-1/3 bg-yellow-100">
+              <div className="text-gray-600 ">P95</div>
+              <div className="font-semibold">${price95}</div>
             </div>
           </div>
         </article>
 
         <article className="flex flex-col w-full items-center justify-center space-y-3">
-          <h2 className="w-full font-semibold rounded-md bg-purple-200 text-center py-1">petrolspy.com</h2>
-          <img className="w-5/6" src="https://petrolspy.com.au/graph/sydney_U91E10.svg" />
+          <h2 className="w-full font-semibold rounded-md bg-gray-200 text-center py-1">Petrol Spy</h2>
+          <img className="" src="https://petrolspy.com.au/graph/sydney_U91E10.svg" />
         </article>
 
         <article className="flex flex-col w-full items-center justify-center space-y-3">
-          <h2 className="w-full font-semibold rounded-md bg-purple-200 text-center py-1">accc.gov.au</h2>
+          <h2 className="w-full font-semibold rounded-md bg-gray-200 text-center py-1">ACCC</h2>
           <img
-            className="w-5/6"
+            className=""
             src="https://www.accc.gov.au/sites/www.accc.gov.au/files/fuelwatch/sydney-ulp.png"
           />
         </article>
       </main>
+
+      <div className="flex justify-center text-center py-10">
+        <a
+          className="hover:underline"
+          href="https://github.com/minho42/petrol"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   );
 }
